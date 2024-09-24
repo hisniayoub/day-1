3ca3e22bb018133accfe16a4b4d5bf591d273e46
hisniayoub <ahisni204@gmail.com>
2024-09-24 20:04:49 +0200

    challenge m

---
+++ conditin3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a , b, r;
+    
+    printf("enter la valeur a :");
+    scanf("%d",&a);
+    printf("enter la valeur b :");
+    scanf("%d",&b);
+    
+    if(a==b){
+ r=a+a+a;
+     printf("la triple de la somme de deux cet valeurs est :%d",r);
+    }
+    
+    
+
+    return 0;
+}
