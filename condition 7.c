47ba05e3bcab8cb8683eba7f1f41fefc68b69ae3
hisniayoub <ahisni204@gmail.com>
2024-09-25 19:25:14 +0200

    challenge m

---
+++ condition 7.c
@@ -0,0 +1,15 @@
+int main() {
+    char a;
+    
+     printf("entre a : ");
+     scanf("%c",&a);
+     if(a >= 'A' && a <= 'Z'){
+     printf("le caractère donné est un alphabet majuscule .");
+     }
+    if(a >= 'a' && a <= 'z'){
+     printf("le caractère donné est un alphabet minuscule");
+    }
+    else 
+    printf(" hada rakm");
+    return 0;
+}
