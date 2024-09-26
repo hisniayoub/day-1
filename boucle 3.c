31ef2d0d967815939d013ecd0cb2b92dc7b4433c
hisniayoub <ahisni204@gmail.com>
2024-09-26 17:57:11 +0200

    challenge m

--- boucle 3.c
+++ boucle 3.c
@@ -2,19 +2,23 @@
 #include <stdlib.h>
 
 int main() {
-    
-    int n ,i, f;
-   
-          printf("programme qui calculer la somme des nombres premiere de n. \n");
+
+    int n ,i, f = 0;
+
+
+
+          printf("programme qui calculer la somme des nombres premiere de n.");
     do{
           printf("veuille entrer le nombre n : ");
           scanf("%d",&n);
-}
+    }
     while(n<0);
-      
-       for(i=1;i<=n;i++)
+
+    for(i=1;i<=n;i++)
         f=f+i;
-        printf("  %d",f);
-    
+
+    printf("  %d",f);
+
+
 return 0;
 }
