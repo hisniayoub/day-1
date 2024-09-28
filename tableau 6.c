207c0786fe2a918ea984b0f0570c8c75acc35cce
hisniayoub <ahisni204@gmail.com>
2024-09-28 17:33:21 +0200

    challenge m

---
+++ tableau 6.c
@@ -0,0 +1,31 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+
+  int taille,n,i,f,p;
+                   printf("Veuillez entrer la taille de tableau ");
+                   scanf("%d",&taille);
+
+
+   int t[taille];
+
+                   printf("dakhl ladd lmdrob fiih ");
+                   scanf("%d",&f);
+
+           for(i=0;i<taille;i++){
+                  printf("entrer n ");
+                  scanf("%d",&n);
+                   t[i]=n;
+
+       }
+
+            for(i=0;i<taille;i++){
+                    p=f* t[i];
+
+                  printf("%d x %d=%d\n ",t[i],f,p);
+        }
+
+
+    return 0;
+}
